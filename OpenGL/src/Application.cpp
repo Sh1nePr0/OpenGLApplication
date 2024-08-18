f19acf8e59433ab632c8f53d564e904859387bb5
Sh1nePr0 <fedoreev77@mail.ru>
2024-08-18 18:38:06 +0300

    render some objects thanks to change model view projection matrix(not the best way, but for now its okay), add one todo

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -55,10 +55,10 @@ int main(void)
 
     {
         float positions[] = {
-			 100.0f, 100.0f, 0.0f, 0.0f,// 0
-			 200.0f, 100.0f, 1.0f, 0.0f,// 1
-			 200.0f,  200.0f,  1.0f, 1.0f,// 2
-			 100.0f,  200.0f,  0.0f, 1.0f // 3
+			 -50.0f, -50.0f, 0.0f, 0.0f,// 0
+			  50.0f, -50.0f, 1.0f, 0.0f,// 1
+			  50.0f,  50.0f,  1.0f, 1.0f,// 2
+			 -50.0f,  50.0f,  0.0f, 1.0f // 3
         };
 
         unsigned int indices[] = {
@@ -85,7 +85,7 @@ int main(void)
         //projection = variable that store values of position of our texture in (-1;1) space left-bottom = -1, right-top = 1
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
         //matrix that represent position of our camera
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100.0f, 0.0f, 0.0f));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
@@ -109,7 +109,9 @@ int main(void)
         ImGui_ImplOpenGL3_Init(glsl_version);
         ImGui::StyleColorsDark();
      
-        glm::vec3 translation(200.0f, 200.0f, 0.0f);
+        glm::vec3 translationA(200.0f, 200.0f, 0.0f);
+
+        glm::vec3 translationB(400.0f, 200.0f, 0.0f);
 
         float r = 0.0f;
         float increment = 0.05f;
@@ -123,16 +125,29 @@ int main(void)
 			ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
-            //matrix that represent moving of our model on screen
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-            //model view projection matrix
-			glm::mat4 mvp = proj * view * model;
-
             shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-            shader.SetUniformMat4f("u_MVP", mvp);
 
-            renderer.Draw(vao, ibo, shader);
+            //TODO:Rewrite in future (Not the best way to render multiply objects)
+            {
+                //matrix that represent moving of our model on screen
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                //model view projection matrix
+                glm::mat4 mvp = proj * view * model;
+				shader.SetUniformMat4f("u_MVP", mvp);
+
+				renderer.Draw(vao, ibo, shader);
+            }
+
+         
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+				glm::mat4 mvp = proj * view * model;
+				shader.SetUniformMat4f("u_MVP", mvp);
+
+				renderer.Draw(vao, ibo, shader);
+			}
+
+
 
             if (r > 1.0f)
                 increment = -0.05f;
@@ -143,7 +158,8 @@ int main(void)
 
 
             {
-                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             }
 
